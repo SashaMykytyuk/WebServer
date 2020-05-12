c15311c0d39b6e94706aefe8aaee24f118769999
SashaMykytyuk <sasha.mykytyuk86@gmail.com>
2020-05-12 15:20:05 +0300

    Added context

---
+++ WebServer/Models/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebServer.Models
+{
+    public class ApplicationContext:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();   // создаем базу данных при первом обращении
+        }
+    }
+}
